f71afbd738b8cba777b5e86be14d50a2968c4742
JulianW03 <woelkjulian@gmail.com>
2026-08-13 16:12:31 +0200

    2 Byte length...

--- backend/src/modules/Valorant/ValorantReplays/storage/import/ReplayFileImportHandler.ts
+++ backend/src/modules/Valorant/ValorantReplays/storage/import/ReplayFileImportHandler.ts
@@ -3,20 +3,26 @@ import { ImportData, ImportHandler } from '@/modules/Valorant/ValorantReplays/st
 import { ReplayFileTypeSchema, ReplayImportRequest } from '#/schemas/upload/ImportReplay.schema';
 import { ReplayMetadataV2 } from '#/schemas/ReplayFormatV2.schema';
 import { CURRENT_REPLAY_FORMAT_VERSION } from '@/modules/Valorant/ValorantReplays/storage/ReplayStorageDTO.schema';
+import { Logger } from '@nestjs/common';
 
 const UUID_OFFSET = 0x30;
-const UUID_LENGTH = 36;
+const UUID_LENGTH = 36 * 2;
 
 export class ReplayFileImportHandler implements ImportHandler {
+    private readonly logger = new Logger(ReplayFileImportHandler.name);
+
     async import(file: Buffer, request: ReplayImportRequest): Promise<ImportData> {
         if (request.type !== ReplayFileTypeSchema.enum.replayFile) {
+            this.logger.error(`ReplayFileImportHandler received unexpected import type: ${request.type}`);
             throw new Error(`ReplayFileImportHandler received unexpected import type: ${request.type}`);
         }
 
         if (file.length < UUID_OFFSET + UUID_LENGTH) {
+            this.logger.error("Unable to extract replay Match UUID");
             throw new Error("Unable to extract replay Match UUID");
         }
         const matchUuid = file.subarray(UUID_OFFSET, UUID_OFFSET + UUID_LENGTH).toString('utf16le');
+        this.logger.debug(`Extracted Match UUID: ${matchUuid}`);
         const checksum = createHash('sha256').update(file).digest('hex');
         const concatId = matchUuid.substring(0, 8);
 
